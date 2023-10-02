e5737e373b7a7c419c53f4d3a1b6481b5a8dd163
m.bebars <66588352+mahmoud-bebars@users.noreply.github.com>
2023-10-02 20:07:33 +0300

    first init

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import "./globals.css";
+export const metadata = {
+  title: "Flexiabble",
+  description: "ShowCase & discover remarkable developers projects",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
